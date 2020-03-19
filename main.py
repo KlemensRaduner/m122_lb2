9d8ae7b4765dd93b523a3e7fe38e2bd01acfc5ca
kleme <klemens.raduner@nyp.ch>
2020-03-19 10:27:29 +0100

    argparse text changed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--Directory", required=True,
                 help="Directory where the files will be copied to")
 ap.add_argument('-l', '--list', action='append',
-                help='<Required> Set flag', required=True)
+                help='fileendings', required=True)
 
 
 args = vars(ap.parse_args())
