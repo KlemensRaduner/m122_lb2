43006d11ae8977d78c467e8195f605dfcc2df5a2
kleme <klemens.raduner@nyp.ch>
2020-03-19 10:46:19 +0100

    changed list to nargs

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--Directory", required=True,
                 help="Directory where the files will be copied to")
-ap.add_argument('-l', '--list', action='append',
+ap.add_argument('-l', '--list', nargs='+',
                 help='fileendings', required=True)
-
 
 args = vars(ap.parse_args())
 
