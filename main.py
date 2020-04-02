bedb0313e69eda825621d9b2adb0a69925adc39e
kleme <klemens.raduner@nyp.ch>
2020-04-02 10:50:16 +0200

    notify

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 from save import Save
+from win10toast import ToastNotifier
+import inotify.adapters
+
+toaster = ToastNotifier()
 
 home = os.path.expanduser('~')
 location = os.path.join(home, 'Downloads')
@@ -7,16 +11,18 @@ folder_check = os.path.isdir(location)
 if folder_check == False:
     quit("no downloads folder")
 
+notifier = inotify.adapters.Inotify()
+notifier.add_watch(location)
+
 f = open("./assignments.lb2", "r")
 saves = []
 for line in f:
     args = line.split()
     saves.append(Save(args[0], args[1:]))
 
-files = os.listdir(location)
-for file in files:
-    name, extension = os.path.splitext(file)
-    for save in saves:
-        if save.hasExtension(extension):
-            os.rename(os.path.join(location, file),
-                      os.path.join(save.dir, name+extension))
+
+for event in notifier.event_gen(yield_nones=False):
+    (_, type_names, path, filename) = event
+
+    print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+        path, filename, type_names))
