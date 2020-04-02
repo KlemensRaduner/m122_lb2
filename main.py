43873522b0e0c4071c14464f68f0612451e20af2
kleme <klemens.raduner@nyp.ch>
2020-04-02 09:19:46 +0200

    find downloads

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+
+home = os.path.expanduser('~')
+location = os.path.join(home, 'Downloads')
+folder_check = os.path.isdir(location)
+if folder_check != True:
+    quit("no downloads folder")
+
 f = open("./assignments.lb2", "r")
 line = f.readline()
 args = line.split()
-
-
-print(args)
