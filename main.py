c3704d14fd6d16d9f2ffa0dba82ad4698476683c
kleme <klemens.raduner@nyp.ch>
2020-04-02 11:11:57 +0200

    gud

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
-from save import Save
+import threading
 from win10toast import ToastNotifier
-import inotify.adapters
+
+
+class Save:
+    def __init__(self, dir, extensions):
+        self.dir = dir
+        self.extensions = extensions
+
+    def hasExtension(self, e):
+        return e in self.extensions
+
 
 toaster = ToastNotifier()
-
 home = os.path.expanduser('~')
 location = os.path.join(home, 'Downloads')
 folder_check = os.path.isdir(location)
 if folder_check == False:
     quit("no downloads folder")
-
-notifier = inotify.adapters.Inotify()
-notifier.add_watch(location)
 
 f = open("./assignments.lb2", "r")
 saves = []
@@ -21,8 +26,19 @@ for line in f:
     saves.append(Save(args[0], args[1:]))
 
 
-for event in notifier.event_gen(yield_nones=False):
-    (_, type_names, path, filename) = event
+def moveFiles():
+    threading.Timer(1.0, moveFiles).start()
+    files = os.listdir(location)
+    moved = 0
+    for file in files:
+        name, extension = os.path.splitext(file)
+        for save in saves:
+            if save.hasExtension(extension):
+                os.rename(os.path.join(location, file),
+                          os.path.join(save.dir, name+extension))
+                moved += 1
+    if moved:
+        toaster.show_toast("LB2", str(moved) + " files moved")
 
-    print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
-        path, filename, type_names))
+
+moveFiles()
--- save.py
+++
@@ -1,7 +0,0 @@
-class Save:
-    def __init__(self, dir, extensions):
-        self.dir = dir
-        self.extensions = extensions
-
-    def hasExtension(self, e):
-        return e in self.extensions
